486ca09fb59a0366c01500c3a1babcb0897ea1b9
CyrusRepaso-UCM <gabutancyrus@gmail.com>
2025-09-16 08:44:56 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,14 @@
+public class Comments {
+    public static void main(String[] args) {
+        // Single-line comment
+
+        /* Multi-line 
+           comment */
+
+        /**
+         * This is a documentation comment
+         *         
+         */
+        System.out.println("Java Comments");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Listen 'cause I'm preachin' to the choir");
+        System.out.println("Can I get the mic a little higher? ");
+        System.out.println("Gimme your desire");
+        System.out.println("I can be the star you rely on (You rely on)");
+    }
+}
